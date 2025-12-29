e2ae00c47899a8007989809341de91e4a8f2c4ea
Erin <enguy4@ocdsb.ca>
2025-12-29 00:27:42 -0500

    initial clicker game has been made + a bunch of autosave experimentation!
    Autosaveeeee

--- clicker-gameeee/Scenes/Protoypes/Clicker/clicker.gd
+++ clicker-gameeee/Scenes/Protoypes/Clicker/clicker.gd
@@ -2,17 +2,25 @@ class_name ProtoypeClicker
 extends Control
 var save_path = "user://variablesave"
 @export var label = Label
+var start_time: int = 0
+
 
 var ivans: int = 0
 
 func _ready() -> void:
+	load_data()
 	update_label_text()
-	print(get_path())
-
+	start_time = Time.get_ticks_msec()
+	
+func _process(delta):
+	var currentime = Time.get_ticks_msec()	
+	var elapsedtime = currentime - start_time
+	var elapsedtimeseconds = elapsedtime/1000
+	if elapsedtimeseconds/10 == 0:
+		save()
 	
 func create_ivans() -> void:
 	ivans += 1
-	print(ivans)
 	update_label_text()
 
 func update_label_text() -> void:
@@ -35,3 +43,8 @@ func load_data():
 		ivans = file.get_var(ivans)
 	else:
 		print("no loaded data")
+		
+		
+func delete_save_data():
+	ivans = 0
+	
--- clicker-gameeee/Scenes/Protoypes/Starting screen/starting_screen.gd
+++ clicker-gameeee/Scenes/Protoypes/Starting screen/starting_screen.gd
@@ -1,18 +1,13 @@
 extends Control
+var deleteall = true
+func _on_load_pressed():
+	get_tree().call_group("clicker","load_data")
+	get_tree().change_scene_to_file("res://Scenes/Protoypes/Clicker/clicker.tscn")
 
-
-@export var game_node: ProtoypeClicker
-
-func _ready() -> void:
-	game_node = get_node("res://Scenes/Protoypes/Clicker/clicker.gd")
-
-func _on_load_pressed() -> void:
-	game_node.load_data()
-	game_node.visible = true
-	self.visible = false
-	
 
 
 func _on_save_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Protoypes/Clicker/clicker.tscn")
+	
+	
 	
